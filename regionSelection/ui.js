ea9a9d4e0d8327f45c8fe9c69f10c98f03026da9
Erik Quinn <erikquinn2@gmail.com>
2016-02-21 16:53:46 -0500

    save work    should probably juist delete me-- I'm aimless

---
+++ regionSelection/ui.js
@@ -0,0 +1,8 @@
+function update() {
+	$('.map').maphilight({fade: false, fillColor:''});
+	$('#na').click(function() {
+		alert( "Handler for 'na' called." );
+	});
+}
+
+$("#shit").mouseover(function() {alert("activating!");});
